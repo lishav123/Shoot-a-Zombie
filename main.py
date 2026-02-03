b1261140dd81f53bfd2f333458077efc46baedd8
lishav123 <aishwaraytiwary@gmail.com>
2026-02-03 22:37:08 +0530

    feat: hide the cursor

--- main.py
+++ main.py
@@ -7,19 +7,25 @@ window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Shoot a Mole")
 
 game_closed = False
+pygame.mouse.set_visible(False)
+
+fps = pygame.time.Clock()
 while not game_closed:
+    window.fill((0, 0, 0))
+    fps.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_closed = True
 
         if event.type == pygame.KEYDOWN:
-            window.fill((255, 255, 255))
+            if event.key == pygame.K_SPACE:
+                window.fill((255, 255, 255))
+                pygame.display.flip()
 
 
     mouse_x, mouse_y = pygame.mouse.get_pos()
 
-    window.fill((0, 0, 0))
-    pygame.draw.circle(window, (255, 0, 0), (mouse_x, mouse_y), 10)
+    pygame.draw.circle(window, (255, 0, 0), (mouse_x, mouse_y), 3)
 
     pygame.display.flip()
 
