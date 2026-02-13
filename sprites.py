62ada826b8204e7566283093258a5493ffdb1f35
lishav123 <aishwaraytiwary@gmail.com>
2026-02-13 21:46:15 +0530

    Added the skeleton of the Zombie

--- sprites.py
+++ sprites.py
@@ -1,7 +1,38 @@
+from pygame import transform
+from pygame import image
+
 class Sprites:
-    def __init__(self, window, x, y):
-        self.window = window
-        self.x = x
+    def __init__(self, window, x, y, scale, sprite, location):
+        self.window  = window
+        self.x       = x
+        self.y       = y
+        self.scale   = scale
+        self._index  = 0
+        self.sprite = sprite
+        self.state   = sprite.IDLE
+        self.location = location
+        self.object = None
+
+        self._loop_stack = []
+
+    def change_position(self, x, y):
+        self.x = x 
         self.y = y
 
-        
+    def display_state(self, flip=False):
+        try:
+            self.state[self._index]
+
+        except IndexError:
+            self._index = 0
+
+        finally:
+            img = image.load(f"{self.location}/{self.state[self._index]}").convert()
+                
+            resize = transform.scale(img, (img.get_width () * self.scale, img.get_height() * self.scale))
+            self.object = transform.flip(resize, flip, False)
+            self.window.blit(self.object, (self.x, self.y))
+
+            if self._index == len(self.state) - 1 and self._loop_stack:
+                self._index = 0
+            self._index += 1
